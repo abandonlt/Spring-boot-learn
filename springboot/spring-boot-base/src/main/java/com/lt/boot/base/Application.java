5014977b95ac6d98e5f632212c602cf8667ab95a
abandom_L <tj_grgzlt@163.com>
2017-10-05 09:49:12 +0800

    create springboot

---
+++ springboot/spring-boot-base/src/main/java/com/lt/boot/base/Application.java
@@ -0,0 +1,13 @@
+package com.lt.boot.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class Application 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
